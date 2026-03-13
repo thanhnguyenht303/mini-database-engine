db1c9f905b5ed722a7b97597306788ddbeae345b
thanhnguyenht303 <nguyenhuutienthanh2002@gmail.com>
2026-03-13 02:23:11 -0500

    adding btree

--- include/btree.hpp
+++ include/btree.hpp
@@ -1,3 +1,39 @@
 #include "pager.hpp"
 #include "row.hpp"
 
+#include <array>
+#include <cstddef>
+#include <cstdint>
+
+enum class NodeType : std::uint8_t {
+    internal = 0,
+    leaf = 1
+};
+
+constexpr std::size_t NODE_TYPE_SIZE = sizeof(std::uint8_t);
+constexpr std::size_t IS_ROOT_SIZE = sizeof(std::uint8_t);
+constexpr std::size_t NUM_CELLS_SIZE = sizeof(std::uint32_t);
+
+constexpr std::size_t NODE_TYPE_OFFSET = 0;
+constexpr std::size_t IS_ROOT_OFFSET = NODE_TYPE_OFFSET + NODE_TYPE_SIZE;
+constexpr std::size_t NUM_CELLS_OFFSET = IS_ROOT_OFFSET + IS_ROOT_SIZE;
+
+constexpr std::size_t LEAF_NODE_HEADER_SIZE = NODE_TYPE_SIZE + IS_ROOT_SIZE + NUM_CELLS_SIZE;
+
+constexpr std::size_t LEAF_NODE_KEY_SIZE = sizeof(std::uint64_t);
+constexpr std::size_t LEAF_NODE_VALUE_SIZE = ROW_SIZE;
+constexpr std::size_t LEAF_NODE_CELL_SIZE = LEAF_NODE_KEY_SIZE + LEAF_NODE_VALUE_SIZE;
+
+constexpr std::size_t LEAF_NODE_SPACE_FOR_CELLS = PAGE_SIZE - LEAF_NODE_HEADER_SIZE;
+
+constexpr std::size_t LEAF_NODE_MAX_CELLS = LEAF_NODE_SPACE_FOR_CELLS / LEAF_NODE_CELL_SIZE;
+
+void initialize_leaf_node(std::array<char, PAGE_SIZE>& page, bool is_root);
+NodeType get_node_type(const std::array<char, PAGE_SIZE>& page);
+std::uint32_t leaf_node_sum_cells(const std::array<char, PAGE_SIZE>& page);
+void set_leaf_node_num_cells(std::array<char, PAGE_SIZE>& page, std::uint32_t count);
+
+std::uint64_t leaf_node_key(const std::array<char, PAGE_SIZE>& page, std::size_t cell_num);
+Row leaf_node_value(const std::array<char, PAGE_SIZE>& page, std::size_t cell_num);
+
+void leaf_node_insert(std::array<char, PAGE_SIZE>& page, const Row& row);
---
+++ src/btree.cpp
@@ -0,0 +1,91 @@
+#include "btree.hpp"
+
+#include <cstring>
+#include <stdexcept>
+
+namespace {
+    std::size_t leaf_node_cell_offset(std::size_t cell_num) {
+        return LEAF_NODE_HEADER_SIZE + cell_num * LEAF_NODE_CELL_SIZE
+    }
+
+    std::size_t leaf_node_key_offset(std::size_t cell_num) {
+        return leaf_node_cell_offset(cell_num);
+    }
+
+    std::size_t leaf_node_value_offset(std::size_t cell_num) {
+        return leaf_node_cell_offset(cell_num) + LEAF_NODE_KEY_SIZE;
+    }
+}
+
+void initialize_leaf_node(std::array<char, PAGE_SIZE>& page, bool is_root) {
+    page.fill(0);
+
+    const std::uint8_t type = static_cast<std::uint8_t>(NodeType::leaf);
+    const std::uint8_t root = is_root ? 1 : 0;
+    const std::uint32_t count = 0;
+
+    std::memcpy(page.data() + NODE_TYPE_OFFSET, &type, NODE_TYPE_SIZE);
+    std::memcpy(page.data() + IS_ROOT_OFFSET, &root, IS_ROOT_SIZE);
+    std::memcpy(page.data() = NUM_CELLS_OFFSET, &count, NUM_CELLS_SIZE);
+}
+
+NodeType get_node_type(const std::array<char, PAGE_SIZE>& page) {
+    std::uint8_t type = 0;
+    std::memcpy(&type, page.data() + NODE_TYPE_OFFSET, NODE_TYPE_SIZE);
+    return static_cast<NodeType>(type);
+}
+
+std::uint32_t leaf_node_num_cells(const std::array<char, PAGE_SIZE>& page){
+    std::uint32_t count = 0;
+    std::memcpy(&count, page.data() + NUM_CELLS_OFFSET, NUM_CELLS_SIZE);
+    return count;
+}
+
+void set_leaf_node_num_cells(std::array<char, PAGE_SIZE>& page, std::uint32_t count) {
+    std::memcpy(page.data() + NUM_CELLS_OFFSET, &count, NUM_CELLS_SIZE);
+}
+
+std::uint64_t leaf_node_key(const std::array<char, PAGE_SIZE>& page, std::size_t cell_num) {
+    std::uint64_t key = 0;
+    std::memcpy(&key, page.data() + leaf_node_key_offset(cell_num), LEAF_NODE_KEY_SIZE);
+    return key;
+}
+
+Row leaf_node_value(const std::array<char, PAGE_SIZE>& page, std::size_t cell_num) {
+    std::array<char, ROW_SIZE> raw{};
+    std::memcpy(raw.data(), page.data() + leaf_node_value_offset(cell_num), ROW_SIZE);
+    return deserialize_row(raw);
+}
+
+void leaf_node_insert(std::array<char, PAGE_SIZE>& page, const Row& row) {
+    std::uint32_t count = leaf_node_num_cells(page);
+
+    if(count >= LEAF_NODE_MAX_CELLS) {
+        throw std::runtime_error("leaf node full");
+    }
+
+    std::size_t index = 0;
+    while (index < count && leaf_node_key(page, index) < row.id) {
+        ++index;
+    }
+
+    if(index < count && leaf_node_key(page, index) == row.id) {
+        throw std::runtime_error("duplicate id");
+    }
+
+    for (std::size_t i = count; i > index; --i){
+        std::memmove(
+            page.data() + leaf_node_cell_offset(i),
+            page.data() + leaf_node_cell_offset(i-1),
+            LEAF_NODE_CELL_SIZE
+        );
+    }
+
+    std::array<char, ROW_SIZE> raw{};
+    serialize_row(row, raw);
+
+    std::memcpy(page.data() + leaf_node_key_offset(index), &row.id, LEAF_NODE_KEY_SIZE);
+    std::memcpy(page.data() + leaf_node_value_offset(index), raw.data, ROW_SIZE);
+
+    set_leaf_node_num_cells(page, count + 1);
+}
